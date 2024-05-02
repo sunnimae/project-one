b373dc2f8293fa65eb2e216188a278189f396c54
blakecbartlett <162613584+blakecbartlett@users.noreply.github.com>
2024-05-02 11:07:04 -0500

    Update clean_data.py

---
+++ clean_data.py
@@ -0,0 +1,10 @@
+#As a group, we faced many hurdles in celaning data.
+#Our data came from: https://www.dps.texas.gov/section/crime-records/crime-texas
+#We took all 10 Excell files from the years 2013-2022 and turned them into csv files.
+#Unfortunately, there was just too much extrenious data in the data sets, it was almost impossible to sift through.
+#We then took the data exclusivley for our city's counties and put them into seperate csv files.
+#This still maintained as an issue for our group as a whole.
+#After trying to sift through the 10 csv files, we still were running into errors.
+#The final solution was brought forth by Victor, in which he stated we should just put all relevant data into one clean csv file.
+#As a group, we were hesistant becasue we wanted to show off our technical skills of being able to read and merge 10 different files.
+#However, in the end, it was the right move as it allowed for us to read, combine, and investigate our research questions with the complete data.
